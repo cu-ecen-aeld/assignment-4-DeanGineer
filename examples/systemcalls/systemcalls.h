1318423fef9180e27f0e6c5be0a0fd87e782d32c
DeanGineer <Deniel.Molina.Careers@gmail.com>
2025-01-20 22:38:49 +0800

    Added C standard libraries to remove compilation errors

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+//added to fix implicit declaration
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h> // For open and file control options 
+#include <sys/wait.h> // For waitpid
 
 bool do_system(const char *command);
 
